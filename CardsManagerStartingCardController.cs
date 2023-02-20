9a1d20e41a2df3483f56ea00253cd10e4e9107db
UrFriendKen <51278666+UrFriendKen@users.noreply.github.com>
2023-02-20 16:49:05 +0800

    Fixed issue causing specials cards to not be affected by setting in PlateUp! v1.1.4

---
+++ CardsManagerStartingCardController.cs
@@ -0,0 +1,44 @@
+﻿using Kitchen;
+using KitchenData;
+using Unity.Entities;
+using static Kitchen.CreateCardSetsFromSetting;
+
+namespace KitchenCardsManager
+{
+    [UpdateInGroup(typeof(ChangeModeGroup))]
+    [UpdateBefore(typeof(CreateCardSetsFromSetting))]
+    internal class CardsManagerStartingCardController : GameSystemBase
+    {
+        public class StartingUnlockData
+        {
+            public RestaurantSetting RestaurantSetting;
+            public Unlock StartingUnlock;
+        }
+
+        public static StartingUnlockData Output;
+
+        protected override void Initialise()
+        {
+            base.Initialise();
+        }
+
+        protected override void OnUpdate()
+        {
+            if (!Has<CHasCreated>() && RequireEntity<SLayout>(out var comp) && Require(comp, out CSetting comp2) && GameData.Main.TryGet<RestaurantSetting>(comp2.RestaurantSetting, out var output))
+            {
+                if (GameData.Main.TryGet<RestaurantSetting>(comp2.RestaurantSetting, out var output2))
+                {
+                    Output = new StartingUnlockData()
+                    {
+                        RestaurantSetting = output,
+                        StartingUnlock = output.StartingUnlock
+                    };
+                }
+            }
+            else
+            {
+                Output = null;
+            }
+        }
+    }
+}
---
+++ Patches/CreateCardSetsFromSetting_Patch.cs
@@ -0,0 +1,60 @@
+﻿using HarmonyLib;
+using Kitchen;
+using KitchenCardsManager.Helpers;
+
+namespace KitchenCardsManager.Patches
+{
+    [HarmonyPatch]
+    internal static class CreateCardSetsFromSetting_Patch
+    {
+        [HarmonyPatch(typeof(CreateCardSetsFromSetting), "OnUpdate")]
+        [HarmonyPrefix]
+        public static bool OnUpdate_Prefix(ref CardsManagerStartingCardController.StartingUnlockData __state)
+        {
+            CardsManagerStartingCardController.StartingUnlockData data = CardsManagerStartingCardController.Output;
+            __state = null;
+            if (data != null)
+            {
+                if (!Main.BlacklistModeEnabled)
+                {
+                    Main.LogInfo("Skipping Starting Card Check due to Vanilla Mode");
+                    return true;
+                }
+
+                string settingName = data.RestaurantSetting.Name;
+                if (data.StartingUnlock == null)
+                {
+                    Main.LogInfo($"No StartingUnlock for setting ({settingName})");
+                    return true;
+                }
+
+                string unlockName = data.StartingUnlock.Name;
+                bool unlockEnabled = UnlockHelpers.GetEnabledState(data.StartingUnlock);
+                if (unlockEnabled)
+                {
+                    Main.LogInfo($"StartingUnlock ({unlockName}) is enabled. Skipping.");
+                    return true;
+                }
+
+                Main.LogInfo($"Removed StartingUnlock ({unlockName})");
+                data.RestaurantSetting.StartingUnlock = null;
+                __state = data;
+                return true;
+            }
+            return false;
+        }
+
+        [HarmonyPatch(typeof(CreateCardSetsFromSetting), "OnUpdate")]
+        [HarmonyPostfix]
+        public static void OnUpdate_Postfix(ref CardsManagerStartingCardController.StartingUnlockData __state)
+        {
+            if (__state == null)
+            {
+                return;
+            }
+
+            __state.RestaurantSetting.StartingUnlock = __state.StartingUnlock;
+            Main.LogInfo($"Replace StartingUnlock with original value ({__state.StartingUnlock.Name})");
+        }
+    }
+}
